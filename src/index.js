fa0aafd7013af06d3fe7166ad751176642da0ebe
AndreySomov <46813938+AndreySomov@users.noreply.github.com>
2019-02-20 00:24:35 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-module.exports = function getZerosCount(number, base) {
-  // your implementation
+module.exports = function getZerosCount(n, k) {
+  let obj = {}, result=n, cloneK=k;
+  for(let i=2; i<=cloneK; i++){
+    while(cloneK%i==0){
+      obj[i] ? obj[i]++ : obj[i]=1;
+      cloneK/=i;
+    }
+  }
+  for(let key in obj){
+    let cloneN = n, res=0;
+    while(cloneN){
+      res += Math.floor(cloneN/+key);
+      cloneN = Math.floor(cloneN/+key) 
+    }
+    result=Math.min(result, Math.floor(res/obj[key]))
+  }
+  return result;
 }
